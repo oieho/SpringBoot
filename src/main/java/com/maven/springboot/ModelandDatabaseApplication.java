437081943e4cdee191397627ea9103e0a98e9238
User <User@Laptop>
2021-01-17 13:27:31 +0900

    modifies pom.xml and creates MyData(Entity)

---
+++ src/main/java/com/maven/springboot/ModelandDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.maven.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelandDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelandDatabaseApplication.class, args);
+	}
+
+}
