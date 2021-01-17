09d20ecf43b6f982cbca29970b7b3537736a5949
oiehomail@gmail.com <oiehomail@gmail.com>
2021-01-17 22:18:13 +0900

    index and Controller are modified.

--- src/main/java/com/maven/springboot/defaultController.java
+++ src/main/java/com/maven/springboot/defaultController.java
@@ -2,22 +2,35 @@ package com.maven.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.maven.springboot.repositories.MyDataRepository;
 
 @Controller
 public class defaultController {
+	
 	@Autowired
 	MyDataRepository repository;
 	
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("msg","this is sample content.");
 		Iterable<MyData> list = repository.findAll();
-		mav.addObject("data", list);
+		mav.addObject("datalist", list);
 		return mav;
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView form(
+			@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
+		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
 	}
 
 }
