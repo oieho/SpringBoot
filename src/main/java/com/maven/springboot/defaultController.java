23209d44c9f9e8041804b5499b140af5c54fdecc
oiehomail@gmail.com <oiehomail@gmail.com>
2021-01-21 15:34:29 +0900

    @ModelAttribute annotation and repository.saveAndFlush method are added to Controller

--- src/main/java/com/maven/springboot/defaultController.java
+++ src/main/java/com/maven/springboot/defaultController.java
@@ -1,5 +1,7 @@
 package com.maven.springboot;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,24 +14,45 @@ import com.maven.springboot.repositories.MyDataRepository;
 
 @Controller
 public class defaultController {
-	
+
 	@Autowired
 	MyDataRepository repository;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView index(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg","this is sample content.");
+		mav.addObject("msg", "this is sample content.");
 		Iterable<MyData> list = repository.findAll();
 		mav.addObject("datalist", list);
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	@Transactional(readOnly=false)
+	@Transactional(readOnly = false)
 	public ModelAndView form(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
 		repository.saveAndFlush(mydata);
 		return new ModelAndView("redirect:/");
 	}
 
+	@PostConstruct
+	public void init() {
+		MyData d1 = new MyData();
+		d1.setName("kim");
+		d1.setAge(123);
+		d1.setMail("kim@gilbut.co.kr");
+		d1.setMemo("this is my data!");
+		repository.saveAndFlush(d1);
+		MyData d2 = new MyData();
+		d2.setName("lee");
+		d2.setAge(15);
+		d2.setMail("lee@flower");
+		d2.setMemo("my girl friend");
+		repository.saveAndFlush(d2);
+		MyData d3 = new MyData();
+		d3.setName("choi");
+		d3.setAge(37);
+		d3.setMail("choi@happy");
+		d3.setMemo("my work friend...");
+		repository.saveAndFlush(d3);
+	}
 }
