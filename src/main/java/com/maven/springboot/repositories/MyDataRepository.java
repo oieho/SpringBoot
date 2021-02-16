af8b955761891abb2d3ca7a93fbf5183ad1bf29e
oieho <oiehomail@gmail.com>
2021-02-16 15:32:27 +0900

    example of implementing JpaRepository method.

--- src/main/java/com/maven/springboot/repositories/MyDataRepository.java
+++ src/main/java/com/maven/springboot/repositories/MyDataRepository.java
@@ -1,5 +1,7 @@
 package com.maven.springboot.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import com.maven.springboot.MyData;
 @Repository
 public interface MyDataRepository extends JpaRepository<MyData, Long>{
 	public MyData findById(long name);
+	public List<MyData> findByNameLike(String name);
+	public List<MyData> findByIdIsNotNullOrderByIdDesc();
+	public List<MyData> findByAgeGreaterThan(Integer age);
+	public List<MyData> findByAgeBetween(Integer age1, Integer age2);
 }
