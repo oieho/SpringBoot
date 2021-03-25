b493c78b66287b347fc29099e5d75842c2735a85
oieho <oiehomail@gmail.com>
2021-03-25 11:06:08 +0900

    query.select(root).orderBy(builder.asc(root.get("name")));

--- src/main/java/com/maven/springboot/MyDataDaoImpl.java
+++ src/main/java/com/maven/springboot/MyDataDaoImpl.java
@@ -34,7 +34,7 @@ public class MyDataDaoImpl implements MyDataDao<MyData> {
 		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<MyData> query = builder.createQuery(MyData.class);
 		Root<MyData> root = query.from(MyData.class);
-		query.select(root);
+		query.select(root).orderBy(builder.asc(root.get("name")));
 		list = (List<MyData>)entityManager.createQuery(query).getResultList();
 		return list;
 	}
